2c85ddd4ef1baba3df917918a8da633ed2470391
Olle Johansson <Olle@Johansson.com>
2010-05-03 14:16:22 +0200

    Updated MurphyPlugin to new command handling.

--- MurphyPlugin.py
+++ MurphyPlugin.py
@@ -12,8 +12,12 @@ class MurphyPlugin(Plugin):
     name = "MurphyPlugin"
     author = "Olle Johansson <Olle@Johansson.com>"
     description = "Murphy bot prints a random quote from Murphy's Rules."
-    command = '!murphy'
-    nick = "HALiBot"
+    commands = [
+        dict(
+            command = ['!murphy'],
+            handler = 'send_quote',
+        ),
+    ]
 
     def setup(self):
         """
@@ -21,28 +25,13 @@ class MurphyPlugin(Plugin):
         """
         self.quotes = read_file("extras/murphy.dat", "%")
 
-    def handleXmppMessage(self, message):
-        """
-        Method called on every received XMPP message stanza.
-        """
-        body = getElStr(message.body)
-        self.send_quote(body, message['nick'])
-
-    def handleShoutMessage(self, shout):
-        """
-        Method called on every new message from the Shoutbox.
-        """
-        self.send_quote(shout.text, shout.name)
-
-    def send_quote(self, text, nick):
+    def send_quote(self, text, nick, command, cmd):
         """
         Parse message body and send message with dice roll.
         """
-        self.logprint("MurphyPlugin: Handling message:", nick, text)
         if not self.quotes:
             return
-        if self.command == '' or text.startswith(self.command):
-            self.bridge.send_and_shout(random.choice(self.quotes), self.nick)
+        self.bridge.send_and_shout(random.choice(self.quotes), self.nick)
 
 def main():
     import sys
@@ -52,8 +41,8 @@ def main():
     import Shoutbox
     cfg = Conf('config.ini', 'LOCAL')
     args = sys.argv
-    msg = ' '.join(args[1:])
-    shout = Shoutbox.Shout(1, 4711, 'Test', msg, time())
+    msg = unicode(' '.join(args[1:]), 'utf-8')
+    shout = Shoutbox.Shout(1, 4711, 'TestUser', msg, time())
     bridge = FakeBridge()
     plug = MurphyPlugin([bridge])
     plug.setup()

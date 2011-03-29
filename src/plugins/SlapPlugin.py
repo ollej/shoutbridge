cecc0454d1212767df3e51a716ab8c398a14ab73
Olle Johansson <Olle@Johansson.com>
2011-03-29 18:25:50 +0200

    Fixed bug with slap/hug counter not being initialized.

--- src/plugins/SlapPlugin.py
+++ src/plugins/SlapPlugin.py
@@ -91,6 +91,8 @@ class SlapPlugin(Plugin):
 
     def increase_counter(self, value):
         slapcount = self.bridge.db.get_value(value)
+        if not slapcount:
+            slapcount = 0
         slapcount = slapcount + 1
         self.bridge.db.set_value(value, slapcount)
 
